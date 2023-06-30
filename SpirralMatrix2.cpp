6b444d144c3c427464397a5a6173f0f22404d9de
Atish Kumar Kushwaha <62862770+atishbhu@users.noreply.github.com>
2023-06-30 13:25:49 +0530

    Create SpirralMatrix2.cpp

---
+++ SpirralMatrix2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void print(vector<vector<int>> A) {
+    int n = A.size();
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cout<<A[i][j]<<"    ";
+        }
+        cout<<endl;
+    }
+}
+
+void generateSpiralPattern(int n) {
+
+    vector<vector<int>> a(n,vector<int>(n));
+
+    int l = n-1;
+    int value = 1;
+    int x,y;
+    for(int i=0;i<n/2;i++) {
+        x=i,y=i;
+        for(int j=0;j<l;j++) {
+            a[x][y] = value;
+            value++;
+            y++;
+        }
+        for(int j=0;j<l;j++) {
+            a[x][y] = value;
+            value++;
+            x++;
+        }
+        for(int j=0;j<l;j++) {
+            a[x][y] = value;
+            value++;
+            y--;
+        }
+        for(int j=0;j<l;j++) {
+            a[x][y] = value;
+            value++;
+            x--;
+        }
+        l -= 2;
+    }
+
+    if(n%2 != 0) {
+        a[x][y] = value;
+    }
+    print(a);
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    generateSpiralPattern(n);
+    return 0;
+}
